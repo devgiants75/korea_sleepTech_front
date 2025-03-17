89810f1b544ab78a88652fbdc7ecbe247212da36
devgiants75 <devgiants75@naver.com>
2025-03-17 11:08:30 +0900

    20250317 js object

--- d_javascript/f_객체/obj04.js
+++ d_javascript/f_객체/obj04.js
@@ -31,13 +31,15 @@ console.log(computer2); // { name: '엘지' }
 // : 원본 객체의 값을 완전히 복제하여 새로운 객체를 생성
 // - 원본 객체와 복사본이 서로 독립적
 
+//? JSON: JavaScript Object Notation(자바스크립트 객체 표기)
+
 // JSON.parse(JSON.stringfy(object))
 
 // 1) JSON.stringfy(obj)
 // : 전달받은 객체를 JSON 문자열로 변환
 
 // 2) JSON.parse(json)
-// : 전달받은 JSON 문자열을 JS 객체로 변환
+// : 전달받은 JSON 문자열을 JS 객체로 변환 - 새로운 메모리 위치에 할당
 
 let book1 = {
   title: '백설공주',
@@ -52,3 +54,18 @@ book2.title = '신데렐라';
 book2.author = '이도경';
 console.log(book2); // { title: '신데렐라', author: '이도경', publishYear: 2025 }
 console.log(book1); // { title: '백설공주', author: '이승아', publishYear: 2025 }
+
+//? +) 객체 속성 확인 연산자
+
+// 1) in 연산자
+// : 속성이 객체에 존재하는지 확인
+// - boolean 타입으로 값 반환
+console.log('title' in book1); // true
+console.log('publisher' in book1); // false
+console.log('author' in book2); // true
+console.log('hello' in book2); // false
+
+// 2) delete 연산자
+// : 해당 속성을 가진 '키: 값'의 쌍을 삭제
+delete book1.title;
+console.log('title' in book1); // false

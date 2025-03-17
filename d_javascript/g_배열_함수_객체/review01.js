fac85e232a1c4d549dbba23b1f13fcb092af5918
devgiants75 <devgiants75@naver.com>
2025-03-17 11:50:25 +0900

    20250317 js array function object review01

---
+++ d_javascript/g_배열_함수_객체/review01.js
@@ -0,0 +1,17 @@
+// review01.js
+
+//# Todo 리스트 만들기 (할 일 목록 관리 시스템) //
+
+//! Todo 객체 구성
+// - id: 각 할 일의 고유 식별자 (주로 숫자나 문자열로 표현)
+// - content: 할 일의 내용을 문자열로 저장
+// - completed: 할 일의 완료 상태를 나타내는 boolean 데이터
+
+/*
+  Todo 객체 예시
+  let todo = {
+    id: 1,
+    content: '정보처리기능사 필기 원서 접수',
+    completed: false
+  }
+*/

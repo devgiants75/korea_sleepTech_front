fe6ac5ac9f84b9da97d8256cf503e6c917a3bb67
devgiants75 <devgiants75@naver.com>
2025-03-24 11:02:55 +0900

    20250324 ts ts-node

--- f_typescript/src/a_타입스크립트/settings.ts
+++ f_typescript/src/a_타입스크립트/settings.ts
@@ -98,4 +98,89 @@
     : 모든 "의존성" 패키지 제거
     - 특정 패키지 제거 방법
       : npm uninstall 패키지명
+
+& === npm 초기화 ===
+: 새로운 Node.js 프로젝트 시작
+  npm init -y
+
+  ? cf) .json 확장자 파일은 주석 불가!
+
+  package name: 프로젝트의 이름을 입력/ 기본값은 현재 디렉터리의 이름
+  version: 패키지의 시작 버전을 입력/ 기본값은 1.0.0
+  description: 프로젝트의 간단한 설명을 입력
+  entry point: 프로젝트의 메인 파일을 지정/ 기본값은 index.js
+  test command: 테스트를 실행할 때 사용할 명령어를 입력
+  git repository: 프로젝트의 Git 저장소 URL을 입력
+  keywords: 프로젝트를 설명하는 키워드를 쉼표로 구분하여 입력
+  author: 패키지의 저자 정보를 입력
+  license: 사용할 라이센스를 입력/ 기본값은 ISC
+
+! 3. typescript
+: 타입스크립트를 설치 (npm 사용)
+: 타입스크립트 사용을 위해서는 로컬 | 전역 환경에 TS 설치가 필수!
+
+- 전역 설치('g 옵션', global)
+  : 개발자 컴퓨터 전체에 기능 추가
+
+- 프로젝트 별 설치(개발 의존성 설치)
+  : 현재 사용하고 있는 프로젝트 내에 기능 추가
+
+: git bash 사용
+  +) f_typescript (TS 프로젝트 최상단)
+
+  ? npm 사용하여 '타입스크립트 패키지' 설치
+  1) 로컬 설치(프로젝트 내)
+  npm install typescript --save-dev
+    - 해당 명령은 TS 프로젝트의 '개발' 의존성으로 추가
+      (devDependencies에 추가)
+    - 배포 시 포함되지 X
+
+    cf) -D VS -S
+      : devDependencies - 개발과정에서 필요한 패키지들의 목록(-D)
+      : dependencies - 프로젝트 실행에 필요한 패키지들의 목록(-S)
+
+  2) 전역 설치(컴퓨터 내)
+  npm install -g typescript
+
+  ? 프로젝트 내의 타입스크립트 버전 확인(설치 확인)
+  tsc -v
+
+! 4. tsc
+: typescript complier
+: TS(.ts) 파일을 JS(.js) 파일로 변환하는 도구
+- 브라우저와 Node.js가 "TS 파일을 직접 실행 X" 때문에 tsc로 코드 변환이 필수!
+
+? tsc 기본 명령어
+- tsc 파일명.ts
+  : 동일한 파일명의 .js 파일이 생성
+  : 해당 .js파일은 TS 코드에서 타입 정보가 제거된 상태
+
+? tsconfig.json 파일
+  : 타입스크립트 프로젝트의 설정을 저장하는 파일
+  : 컴파일 방법이 명시
+
+  npx tsc --init
 */ 
+
+//! 터미널 명령어
+// 터미널 '열기' 단축키
+// : ctrl + shift+ `(백틱)
+
+//? cd 명령어: 사용자가 현재 위치하는 디렉토리(폴더) 변경할 때 사용
+// : change directory
+
+// 1) 특정 디렉토리로 이동(절대 경로)
+// cd 전체_디렉토리_경로
+// EX) c:/이승아/korea_sleepTech_front
+
+// 2) 홈 디렉토리로 이동(프로젝트 최상단)
+// cd | cd ~
+
+// 3) 현재 디렉토리를 기준으로 상위 디렉토리 이동
+// cd ..
+
+// 4) 이전 디렉토리 이동
+// cd - 
+
+// 5) 현재 디렉토리를 기준으로 하위 디렉토리 이동
+// cd 하위/디렉토리/경로

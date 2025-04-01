b5c59ecf34c866035fd3d702565f9fff9a6265ba
devgiants75 <devgiants75@naver.com>
2025-04-01 12:45:12 +0900

    20250401 react hooks - useState

--- korea-sleepTech-react/src/pages/b_hooks/index.tsx
+++ korea-sleepTech-react/src/pages/b_hooks/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import UseState01 from "./a_useState/UseState01";
 import UseState02 from "./a_useState/UseState02";
 import UseState03 from "./a_useState/UseState03";
+import UseState04 from "./a_useState/UseState04";
 
 const h2Style = {
   backgroundColor: "black",
@@ -24,6 +25,7 @@ function Index() {
       <UseState01 /> <hr />
       <UseState02 /> <hr />
       <UseState03 /> <hr />
+      <UseState04 /> <hr />
     </div>
   );
 }

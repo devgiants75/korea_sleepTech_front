3be42c08bcaded5470f1a3bba3c72746839b7cc1
devgiants75 <devgiants75@naver.com>
2025-04-02 09:48:43 +0900

    20250402 react hooks - useEffect

--- korea-sleepTech-react/src/pages/b_hooks/index.tsx
+++ korea-sleepTech-react/src/pages/b_hooks/index.tsx
@@ -11,6 +11,7 @@ import UseStatePractice from "./a_useState/Practice";
 import UseRef01 from "./b_useRef/UseRef01";
 import UseRef02 from "./b_useRef/UseRef02";
 import UseRefPractice from "./b_useRef/Practice";
+import UseEffect01 from "./c_useEffect/UseEffect01";
 
 const h2Style = {
   backgroundColor: "black",
@@ -28,6 +29,9 @@ function Index() {
       >
         === 리액트 Hooks ===
       </h1>
+
+      <h2 style={h2Style}>리액트 Hooks - useEffect</h2>
+      <UseEffect01 />
 
       <h2 style={h2Style}>리액트 Hooks - useRef</h2>
       <UseRef01 />

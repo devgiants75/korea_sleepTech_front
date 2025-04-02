51a634ec3b48416b221f6ce5a5317ea80bb59357
devgiants75 <devgiants75@naver.com>
2025-04-02 16:26:05 +0900

    20250402 react hooks - webcam app

--- korea-sleepTech-react/src/pages/b_hooks/index.tsx
+++ korea-sleepTech-react/src/pages/b_hooks/index.tsx
@@ -21,6 +21,7 @@ import UseReducer02 from "./e_useReducer/UseReducer02";
 import Custom01 from "./f_customHook/Custom01";
 import Custom02 from "./f_customHook/Custom02";
 import TodoAppLocalStorage from "./z_todo/TodoAppLocalStorage";
+import Practice01 from "./Practice01";
 
 const h2Style = {
   backgroundColor: "black",
@@ -38,6 +39,18 @@ function Index() {
       >
         === 리액트 Hooks ===
       </h1>
+      <h2 style={h2Style}>리액트 Hooks - Webcam 앱 구현</h2>
+      <div style={{
+        margin: '20px auto',
+        padding: '10px',
+        height: '100vh',
+        backgroundColor: '#f4f4f4',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <Practice01 />
+      </div>
 
       <h2 style={h2Style}>리액트 Hooks - Todo 앱 구현</h2>
       <div style={{

f3da811f38318ffe2e85e428202f7cc63e0a866b
devgiants75 <devgiants75@naver.com>
2025-04-03 12:24:59 +0900

    20250403 react router practice

--- korea-sleepTech-react/src/pages/d_router_practice/Contact.tsx
+++ korea-sleepTech-react/src/pages/d_router_practice/Contact.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+//! 기본 컴포넌트
 function Contact() {
   return (
-    <div>Contact</div>
+    <div>
+      <h2>Contact 문의하기</h2>
+      <p>이메일: devgiants75@naver.com</p>
+    </div>
   )
 }
 
--- korea-sleepTech-react/src/pages/d_router_practice/index.tsx
+++ korea-sleepTech-react/src/pages/d_router_practice/index.tsx
@@ -5,12 +5,20 @@ import About from './About'
 import Contact from './Contact'
 import User from './User'
 import Posts from './Posts'
+import { Link } from 'react-router-dom'
 
 // '/router-practice' 경로
 function Index() {
   return (
     <div>
-      
+      <h2>React Router 실습</h2>
+      <nav>
+        <Link to='/router-practice'>홈</Link> | {" "}
+        <Link to='/router-practice/about'>소개</Link> | {" "}
+        <Link to='/router-practice/contact'>문의하기</Link> | {" "}
+        <Link to='/router-practice/user/1'>1번 유저</Link> | {" "}
+        <Link to='/router-practice/posts?userId=1'>1번 유저 게시글</Link>
+      </nav>
 
       <Routes>
         <Route path='/' element={<Home />} />

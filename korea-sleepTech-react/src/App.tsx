6883d28f740b449326a2587e697386367d83c9dc
devgiants75 <devgiants75@naver.com>
2025-04-07 11:47:18 +0900

    20250407 react http & axios

--- korea-sleepTech-react/src/App.tsx
+++ korea-sleepTech-react/src/App.tsx
@@ -12,6 +12,7 @@ import Hooks from '@/pages/b_hooks';
 import Router from '@/pages/c_router';
 import RouterPractice from '@/pages/d_router_practice';
 import Review from '@/pages/e_review';
+import Http from '@/pages/f_http';
 
 //& 기본 Vite React 앱의 경로
 // : http://localhost:5173
@@ -40,6 +41,7 @@ function App() {
         <Route path='/router/*' element={<Router />} />
         <Route path='/router-practice/*' element={<RouterPractice />} />
         <Route path='/review/*' element={<Review />} />
+        <Route path='/http' element={<Http />} />
       </Routes>
     </>
   )
